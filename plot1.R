cc550e943f8199d2cdde08db46ae781c68f16821
Ming <pioneery@users.noreply.github.com>
2015-02-08 15:36:08 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,5 +4,5 @@ data1 <- subset(data, data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 data1$Global_active_power <- as.numeric(as.character(data1$Global_active_power))
 hist(data1$Global_active_power, col ="red",main= "Global Active Power",
      xlab="Global Active Power(kilowatts)")
-dev.copy(png,filename="plot1.png")
+dev.copy(png,filename="plot1.png",width = 480, height = 480, units = "px")
 dev.off ()
